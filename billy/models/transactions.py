df6602949064d6cd3b85527e9386811e21bd7d2a
Parham Negahdar <pnegahdar@gmail.com>
2013-06-06 14:26:22 -0700

    Updated transactions method.

--- billy/models/transactions.py
+++ billy/models/transactions.py
@@ -8,24 +8,38 @@ from billy.utils.models import uuid_factory
 
 
 class Transaction(Base):
-
     external_id = Column(Unicode, ForeignKey)
     group_id = Column(Unicode, ForeignKey('groups.guid'))
     customer_id = Column(Unicode, ForeignKey('customers.external_id'))
     created_at = Column(DateTime(timezone=UTC))
     amount_cents = Column(Integer)
 
+    @classmethod
+    def create(cls, external_id, group_id, customer_id, amount_cents):
+        new_transaciton = cls(
+            external_id=external_id,
+            group_id=group_id,
+            customer_id=customer_id,
+            amount_cents=amount_cents,
+        )
+        cls.session.add(new_transaciton)
+        cls.session.commit()
 
 
-
+    @classmethod
+    def retrieve(cls, group_id, customer_id=None, external_id=None):
+        query = cls.query.filter(cls.group_id == group_id)
+        if customer_id:
+            query.filter(cls.customer_id == customer_id)
+        if external_id:
+            query.filter(cls.external_id == external_id)
+        return query.all()
 
 
 class PaymentTransaction(Transaction):
     __tablename__ = 'payout_transactions'
 
     guid = Column(Unicode, primary_key=True, default=uuid_factory('PAT'))
-
-
 
 
 class PayoutTransaction(Transaction):

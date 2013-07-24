1f2429d805fec2fbf7b4d8778e0a2a4308ff8bf1
Parham Negahdar <pnegahdar@gmail.com>
2013-07-24 14:13:56 -0700

    lunch

--- billy/api/errors/definitions.py
+++ billy/api/errors/definitions.py
@@ -1,6 +1,12 @@
 from __future__ import unicode_literals
 
 errors = {
+    # GENERIC ERRORS
+    '400': {
+        'status': 400,
+        'message': 'Please check your request parameters.'
+    },
+
     # GROUP ERRORS
     '401': {
         'status': 401,
@@ -16,6 +22,11 @@ errors = {
         'status': 409,
         'error_message': 'Cannot perform POST on an existing customer. Use '
                          'PUT instead.'
+    },
+
+    '400_CUSTOMER_ID': {
+        'status': 400,
+        'error_message': 'Invalid customer_id. Please check.'
     }
 
 }

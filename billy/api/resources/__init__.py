5787ef746ec05961fd5c7ec0761651510d48d560
Parham Negahdar <pnegahdar@gmail.com>
2013-07-30 12:23:15 -0700

    Moved around imports to clean up the spec

--- billy/api/resources/__init__.py
+++ billy/api/resources/__init__.py
@@ -1,3 +1,31 @@
+# Generic
 from base import Base
-from api.resources.group import GroupController
+
+#Fluff
 from api.resources.base import Home
+
+#API RESOURCES
+from api.resources.group import GroupController
+from api.resources.customer import (CustomerIndexController,
+                                    CustomerController, customer_view)
+from api.resources.coupon import (CouponIndexController, CouponController,
+                                  coupon_view)
+from api.resources.plan import (PlanIndexController, PlanController, plan_view)
+from api.resources.payout import (PayoutIndexController, PayoutController,
+                                  payout_view)
+from api.resources.plan_subscription import (PlanSubIndexController,
+                                             PlanSubController, plan_sub_view)
+from api.resources.payout_subscription import (PayoutSubIndexController,
+                                               PayoutSubController,
+                                               payout_sub_view)
+from api.resources.plan_invoice import (PlanInvController,
+                                        PlanInvIndexController, plan_inv_view)
+from api.resources.payout_invoice import (PayoutInvController,
+                                          PayoutInvIndexController,
+                                          payout_inv_view)
+from api.resources.plan_transaction import (PlanTransIndexController,
+                                            PlanTransController,
+                                            plan_trans_view)
+from api.resources.payout_transaction import (PayoutTransIndexController,
+                                              PayoutTransController,
+                                              payout_trans_view)

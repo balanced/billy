7522759579efbfdd2e059cecf5022238cabc971d
Victor Lin <bornstub@gmail.com>
2013-08-06 23:25:41 +0800

    Add more tests for plan model

--- billy/tests/test_models/test_plan.py
+++ billy/tests/test_models/test_plan.py
@@ -9,18 +9,46 @@ class TestPlanModel(ModelTestCase):
 
     def test_create_plan(self):
         model = self.make_one(self.session)
-        name = 'monthly billing to user john'
+        name = 'monthly billing to user John'
         amount = 5566.77
         frequency = model.FREQ_MONTHLY
         guid = model.create_plan(
             name=name,
             amount=amount,
-            frequency=model.FREQ_MONTHLY,
+            frequency=frequency,
         )
         plan = model.get_plan_by_guid(guid)
         self.assertEqual(plan.guid, guid)
         self.assertEqual(plan.name, name)
         self.assertEqual(plan.amount, amount)
         self.assertEqual(plan.frequency, frequency)
+        self.assertEqual(plan.active, True)
         self.assertEqual(plan.created_at, self.now)
         self.assertEqual(plan.updated_at, self.now)
+
+    def test_create_plan_with_wrong_frequency(self):
+        model = self.make_one(self.session)
+
+        with self.assertRaises(ValueError):
+            model.create_plan(
+                name=None,
+                amount=999,
+                frequency=999,
+            )
+
+    def test_get_plan(self):
+        model = self.make_one(self.session)
+        name = 'evil gangster charges protection fee from Tom weekly'
+        amount = 99.99
+        frequency = model.FREQ_WEEKLY
+        guid = model.create_plan(
+            name=name,
+            amount=amount,
+            frequency=frequency,
+        )
+
+        plan = model.get_plan_by_guid('not-exist')
+        self.assertEqual(plan, None)
+
+        plan = model.get_plan_by_guid(guid)
+        self.assertNotEqual(plan, None)

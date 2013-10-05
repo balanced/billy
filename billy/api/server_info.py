1d7e539f15277ee070257cc5889619eb6d545b45
Victor Lin <bornstub@gmail.com>
2013-10-05 08:07:52 +0800

    Add last transaction created field in server info

--- billy/api/server_info.py
+++ billy/api/server_info.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from pyramid.view import view_config
 
+from billy.models.transaction import TransactionModel
 from billy.utils.generic import get_git_rev
 
 
@@ -9,12 +10,17 @@ from billy.utils.generic import get_git_rev
              request_method='GET', 
              renderer='json')
 def server_info(request):
-    """Get server info
+    """Get server information
 
     """
-    # TODO: get some more useful information here, such as the date time
-    # of last yield transaction?
+    tx_model = TransactionModel(request.session)
+    last_transaction = tx_model.get_last_transaction()
+    last_transaction_dt = None
+    if last_transaction is not None:
+        last_transaction_dt = last_transaction.created_at.isoformat()
     return dict(
-        server='Billy - The recurring payment server powered by Balanced',
+        server='Billy - The recurring payment server',
+        powered_by='BalancedPayments.com',
         revision=get_git_rev(),
+        last_transaction_created_at=last_transaction_dt,
     )
--- billy/tests/functional/test_server_info.py
+++ billy/tests/functional/test_server_info.py
@@ -1,4 +1,7 @@
 from __future__ import unicode_literals
+import datetime
+
+import transaction as db_transaction
 
 from billy.tests.functional.helper import ViewTestCase
 
@@ -12,3 +15,47 @@ class TestServerInfo(ViewTestCase):
     def test_server_info(self):
         res = self.testapp.get('/', status=200)
         self.assertIn('revision', res.json)
+
+    def test_server_info_with_transaction(self):
+        from billy.models.company import CompanyModel
+        from billy.models.customer import CustomerModel
+        from billy.models.plan import PlanModel
+        from billy.models.subscription import SubscriptionModel
+        from billy.models.transaction import TransactionModel
+
+        company_model = CompanyModel(self.testapp.session)
+        customer_model = CustomerModel(self.testapp.session)
+        plan_model = PlanModel(self.testapp.session)
+        subscription_model = SubscriptionModel(self.testapp.session)
+        transaction_model = TransactionModel(self.testapp.session)
+
+        with db_transaction.manager:
+            company_guid = company_model.create(
+                processor_key='MOCK_PROCESSOR_KEY',
+            )
+            customer_guid = customer_model.create(
+                company_guid=company_guid
+            )
+            plan_guid = plan_model.create(
+                company_guid=company_guid,
+                frequency=plan_model.FREQ_WEEKLY,
+                plan_type=plan_model.TYPE_CHARGE,
+                amount=10,
+            )
+            subscription_guid = subscription_model.create(
+                customer_guid=customer_guid,
+                plan_guid=plan_guid,
+            )
+            transaction_guid = transaction_model.create(
+                subscription_guid=subscription_guid,
+                transaction_type=transaction_model.TYPE_CHARGE,
+                amount=10,
+                payment_uri='/v1/cards/tester',
+                scheduled_at=datetime.datetime.utcnow(),
+            )
+
+        transaction = transaction_model.get(transaction_guid)
+
+        res = self.testapp.get('/', status=200)
+        self.assertEqual(res.json['last_transaction_created_at'], 
+                         transaction.created_at.isoformat())

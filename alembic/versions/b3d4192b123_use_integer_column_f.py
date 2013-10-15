2d58926c1127ac46d9d4f58b9e0e96254eb482b9
Victor Lin <bornstub@gmail.com>
2013-10-15 17:00:32 +0800

    Add test for alembic

--- alembic/versions/b3d4192b123_use_integer_column_f.py
+++ alembic/versions/b3d4192b123_use_integer_column_f.py
@@ -10,6 +10,7 @@ Create Date: 2013-10-15 11:42:53.334742
 revision = 'b3d4192b123'
 down_revision = None
 
+import decimal
 from alembic import op
 from sqlalchemy.sql import column
 from sqlalchemy.sql import table
@@ -60,12 +61,13 @@ def downgrade():
         op.alter_column('subscription', 'amount', type_=Numeric(10, 2))
         op.alter_column('transaction', 'amount', type_=Numeric(10, 2))
     # update all amount values from like 12.34 to 1234
+    num = decimal.Decimal('100')
     op.execute(
-        plan.update().values(dict(amount=plan.c.amount / 100))
+        plan.update().values(dict(amount=plan.c.amount / num))
     )
     op.execute(
-        subscription.update().values(dict(amount=subscription.c.amount / 100))
+        subscription.update().values(dict(amount=subscription.c.amount / num))
     )
     op.execute(
-        transaction.update().values(dict(amount=transaction.c.amount / 100))
+        transaction.update().values(dict(amount=transaction.c.amount / num))
     )

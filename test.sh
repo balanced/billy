297a7b35fa90f26a8d5a4162713324ba3f517190
Victor Lin <bornstub@gmail.com>
2014-01-15 11:07:05 +0800

    Adjust testing argument for our own testing pipeline

--- test.sh
+++ test.sh
@@ -4,5 +4,5 @@ if [ -z "$BILLY_TEST_INTEGRATION" ]; then
 else
     initialize_billy_db test.ini
     pserve test.ini --daemon
-    nosetests billy/tests/integration --verbosity=2
+    nosetests billy/tests/integration --verbosity=2 --with-coverage=1
 fi

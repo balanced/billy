4e4368c0cbd397bf6a9e85cb1ca33a2baac64225
Victor Lin <bornstub@gmail.com>
2014-01-15 11:41:14 +0800

    Fix testing

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ -z "$BILLY_TEST_INTEGRATION" ]; then
-    python setup.py nosetests
+    python setup.py nosetests --verbosity=2 --with-coverage
 else
     initialize_billy_db test.ini
     pserve test.ini --daemon
-    nosetests billy/tests/integration --verbosity=2 --with-coverage=1
+    nosetests billy/tests/integration --verbosity=2 --with-coverage
 fi
